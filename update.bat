30ea381dfb5dd8ed19be99b75806d1b39a9388e4
Josef Spjut <jspjut@nvidia.com>
2022-04-11 08:34:27 -0400

    Dark materials

--- update.bat
+++ update.bat
@@ -4,5 +4,6 @@ echo Copying configs over
 xcopy configs\*.Any FPSci\ /Y
 xcopy model\sphere.* FPSci\model\target\ /Y
 
-echo Copying scene file over
+echo Copying scene files over
 xcopy scene\*.Scene.Any FPSci\scene\ /Y
+xcopy material\*.UniversalMaterial.Any FPSci\material\ /Y

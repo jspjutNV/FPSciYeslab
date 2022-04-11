1e083e16d45f45fe0ef44df62f96b01d3ba6b162
Josef Spjut <jspjut@nvidia.com>
2022-04-11 08:23:43 -0400

    Add YESLAB version of hallway scene

--- setup.bat
+++ setup.bat
@@ -14,3 +14,6 @@ del FPSci.%FPSCI_VERSION%.zip
 echo Copying configs over
 xcopy configs\*.Any FPSci\ /Y
 xcopy model\sphere.* FPSci\model\target\ /Y
+
+echo Copying scene file over
+xcopy scene\*.Scene.Any FPSci\scene\ /Y
--- update.bat
+++ update.bat
@@ -3,3 +3,6 @@
 echo Copying configs over
 xcopy configs\*.Any FPSci\ /Y
 xcopy model\sphere.* FPSci\model\target\ /Y
+
+echo Copying scene file over
+xcopy scene\*.Scene.Any FPSci\scene\ /Y
